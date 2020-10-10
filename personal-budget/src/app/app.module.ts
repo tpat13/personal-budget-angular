a34e10ac69d340b863de5437634e05014d5e247f
unknown <tpatil@uncc.edu>
2020-10-10 00:00:47 -0400

    Created Contact

--- personal-budget/src/app/app.module.ts
+++ personal-budget/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
     AboutComponent,
     LoginComponent,
     P404Component,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
